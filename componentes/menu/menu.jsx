1144550ee75e89e4e13054702b03311d78f52eaf
el_tio_dan <xddanieloow@gmail.com>
2024-12-01 07:19:55 -1000

    Menu terminado y estilizado correctamente, este menu es reponsivo y se adapta para cualquier entorno usando una paleta de colores solida

--- componentes/menu/menu.jsx
+++ componentes/menu/menu.jsx
@@ -1,26 +1,67 @@
+"use client"
+import React from "react";
 import "../menu/menu.css";
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+// iconos
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars,faXmark } from '@fortawesome/free-solid-svg-icons';
+import logo from "../../public/logo.jpg";
+
+
+
 
 function Menu() {
+
+    const [showMenu, setShowMenu] = useState(false);
+
+    function showHideMenu(sh) {
+        sh == "s" ?
+            setShowMenu(true):
+            setShowMenu(false)
+    }
+ 
     return (  
         <nav className="menuContent">
-            <div>
-                
+            <button onClick={()=>showHideMenu("s")} className="menuContent__icon">
+                <FontAwesomeIcon icon={faBars} />
+            </button>
+            <div className="menuContent__logo">
+                <Image src={logo} alt="logo" />
             </div>
-            <ul>
-                <li>
-                    <Link href={"/"}>Inicio</Link>
-                </li>
-                <li>
-                    <Link href={"/apuntes"}>Apuntes</Link>
-                </li>
-                <li>
-                    <Link href={"/proyectos"}>Proyectos</Link>
-                </li>
-                <li>
-                    <Link href={"/about"}>Sobre mi</Link>
-                </li>
-            </ul>
+
+            {/* oscurecer y funcion para quitar menu */}
+            <div 
+                onClick={()=>showHideMenu("h")} 
+                className={`menuContent__oscurecer ${!showMenu ? "":"menuContent__oscurecerShow"}`}
+            ></div>
+
+            <div className={`menuContent__nav ${!showMenu ? "":"menuContent__navShow"}`}>
+                <button onClick={()=>showHideMenu("h")} className="menuContent__nav--close">
+                    <FontAwesomeIcon icon={faXmark} />
+                </button>
+                <ul className="menuContent__nav--ul">
+                    <li>
+                        <Link onClick={()=>showHideMenu("h")} href={"/"}>Inicio</Link>
+                    </li>
+                    <li>
+                        <Link onClick={()=>showHideMenu("h")} href={"/apuntes"}>Apuntes</Link>
+                    </li>
+                    <li>
+                        <Link onClick={()=>showHideMenu("h")} href={"/proyectos"}>Proyectos</Link>
+                    </li>
+                    <li>
+                        <Link onClick={()=>showHideMenu("h")} href={"/about"}>Sobre mi</Link>
+                    </li>
+                </ul>
+                <div className="menuContent__nav--data">
+                    <Link href={""}>Registrarse</Link>
+                    <Link href={""}>Iniciar secion</Link>
+                </div>
+            </div>
+
             
         </nav>
     );
