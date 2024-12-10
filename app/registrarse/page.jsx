313b0d39d616f68d4694e1a8814000fea425558a
el_tio_dan <xddanieloow@gmail.com>
2024-12-09 16:26:23 -1000

    se cambio los dos iconos de contraseña y se implemento un validador de contraseña para saber si una contraseña es segura o no

--- app/registrarse/page.jsx
+++ app/registrarse/page.jsx
@@ -1,18 +1,25 @@
 "use client";
 import "@/app/registrarse/registrarse.css";
-import { faCheck, faEnvelope, faLock, faUser, faX } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faEnvelope, faEye, faEyeLowVision, faLock, faUser, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useState, useRef, useDebugValue } from "react";
 
+
+// validar seguridad de contraseña.
+import zxcvbn from 'zxcvbn';
+
 export default function Registrarse() {
     // Estado de los inputs
     const [inputs, setInputs] = useState(["", "", "", ""]);
-    const [btnClose, setBtnClose] = useState([false, false, false, false]);
-    const labelsRef = useRef([]); // Referencias de los textos indicativos
-    const [showErr, setShowErr] = useState([false,false,false,false]);
-    const [msgError, setMsgError] = useState("asd"); // mensajes de error
-    const [indexInput, setIndexInput] = useState(0); // indices del input que se hace click.
+    const [btnClose, setBtnClose] = useState([false, false]); // botones para eliminar el contenido de algun campo.
+    const [btnEye, setBtnEye] = useState([false,false]); // boton para mostrar o ocultar la contraseña
+    const [btnPasswordIcon, setBtnPasswordIcon] = useState([false,false]); // iconos del boton de password.
+    const inputsPassword = useRef([]); // inputs de contraseña.
+    const labelsRef = useRef([]); // Referencias de los textos indicativos.
+    const [showErr, setShowErr] = useState(false); // mostrar o ocultar error.
+    const [msgError, setMsgError] = useState("asd"); // mensajes de error.
+
 
     // Función para manejar cambios en los inputs
     function manejarInputs(index, value) {
@@ -27,6 +34,14 @@ export default function Registrarse() {
             updated[index] = value !== "";
             return updated;
         });
+
+        setBtnEye((prev) => {
+            const update = [...prev];
+            update[index] = value !== "";
+            return update;
+        })
+
+
     }
 
     // Función para restablecer un input
@@ -35,30 +50,36 @@ export default function Registrarse() {
         manejarInputs(index, "");
     }
 
+    // botones para mostrar o ocultar contraseña
+    function showHidePassword(e, index) {
+        e.preventDefault();
+        e.stopPropagation();
+        const inputsPasswords = document.querySelectorAll(".contentRegister__form--contentInput__input");
+        setBtnPasswordIcon((prev) => {
+            const update = [...prev];
+            update[index] = !update[index];
+            return update;
+        });
+        inputsPasswords[index].value = "text";
+    }
+
     function sendData(e) {
         e.preventDefault();
-        for (let i = 0; i < inputs.length; i++) {
-            setShowErr((err)=>{
-                const update = [...err];
-                update[i] = false;
-                return update
-            })
-            setBtnClose((prev) => {
-                const updated = [...prev];
-                updated[i] = false;
-                return updated;
-            });
-            if (inputs[i] == "") {
-                setMsgError("LLena el espacio.")
-                setShowErr((err)=>{
-                    const update = [...err];
-                    update[i] = true;
-                    return update
-                })
-                break;
-            }
-        }
+        let email = /^[a-zA-Z0-9.]{6,30}@gmail\.com$/;
+        const password = zxcvbn(inputs[2] || inputs[3]);
+        if (inputs[0].length < 5){showHideError(`El nombre debe tener al menos 5 caracteres.`)}
+        else if(!email.test(inputs[1])){showHideError(`El correo es invalido.`)}
+        else if(inputs[2].length < 8 || inputs[3].length < 8){showHideError("La contraseña debe tener al menos 8 caracteres.")}
+        else if(inputs[2] != inputs[3]){showHideError("La segunda contraseña debe ser igual a la primera.")}
+        else if(password.score != 4){showHideError("Tu contraseña es demasiado insegura.")}
+    }
 
+    function showHideError(msg) {
+        setShowErr(true);
+        setMsgError(msg);
+        setTimeout(() => {
+            setShowErr(false);
+        }, 5000);
     }
 
     // Generar dinámicamente los inputs
@@ -66,53 +87,79 @@ export default function Registrarse() {
         { placeholder: "Nombre de usuario", icon: faUser, type: "text" },
         { placeholder: "Email", icon: faEnvelope, type: "text" },
         { placeholder: "Contraseña", icon: faLock, type: "password" },
-        { placeholder: "Confirmar contraseña", icon: faCheck, type: "password" },
+        { placeholder: "Confirmar contraseña", icon: faCheck, type: "password" }
     ];
 
     return (
         <main className="contentRegister">
             <form className="contentRegister__form">
                 <h1 className="contentRegister__form--h1">Registrarse</h1>
-                {inputFields.map((field, index) => (
-                    <div key={index}>
-                        <div
-                            className="contentRegister__form--contentInput"
-                            onClick={() => {
-                                labelsRef.current[index].classList.add("contentRegister__form--contentInput__p--up")
-                                setIndexInput(index)
-                            }}
+                {inputFields.map((field, index) => (      
+                    <div
+                        className="contentRegister__form--contentInput"
+                        onClick={() => {
+                            labelsRef.current[index].classList.add("contentRegister__form--contentInput__p--up")
+                        }}
+                        key={index}
+                    >
+                        <p
+                            ref={(el) => (labelsRef.current[index] = el)}
+                            className={`contentRegister__form--contentInput__p ${
+                                inputs[index] ? "contentRegister__form--contentInput__p--up" : ""
+                            }`}
                         >
-                            <p
-                                ref={(el) => (labelsRef.current[index] = el)}
-                                className={`contentRegister__form--contentInput__p ${
-                                    inputs[index] ? "contentRegister__form--contentInput__p--up" : ""
-                                }`}
-                            >
-                                {field.placeholder}
-                            </p>
-                            <span className="iconRegistenForm">
-                                <FontAwesomeIcon icon={field.icon} />
-                            </span>
-                            <input
-                                className="contentRegister__form--contentInput__input"
-                                type={field.type}
-                                value={inputs[index]}
-                                onChange={(e) => manejarInputs(index, e.target.value)}
-                            />
-                            {btnClose[index] && (
+                            {field.placeholder}
+                        </p>
+                        <span className="iconRegistenForm">
+                            <FontAwesomeIcon icon={field.icon} />
+                        </span>
+                        <input
+                            className="contentRegister__form--contentInput__input"
+                            type={field.type}
+                            value={inputs[index]}
+                            onChange={(e) => manejarInputs(index, e.target.value)}
+                        />
+                        {/* renderizar botones para borrar todo el contenido de un campo */}
+                        {
+                            field.type != "password" && btnClose[index] && (
                                 <button
                                     className="contentRegister__form--contentInput__btnDel"
                                     onClick={(e) => delInput(e, index)}
                                 >
                                     <FontAwesomeIcon icon={faX} />
                                 </button>
-                            )}
-                        </div>
-                        {showErr[index] && (<p
-                            className="contentRegister__form--contentInputErr">{msgError}</p>)}
-                    </div>
+                            )
+                        }
 
+                        {/* renderizar botones para mostrar o ocultar contraseña-
+                         */}
+                        {
+                            field.type == "password" && btnEye[index] && (
+                                <button 
+                                    className="contentRegister__form--contentInput__btnDel"
+                                    onClick={(e)=> showHidePassword(e, index)}
+                                >
+                                    {
+                                        btnPasswordIcon[index] ? 
+                                            <FontAwesomeIcon icon={faEye}/>:
+                                            <FontAwesomeIcon icon={faEyeLowVision}/>
+                                    }
+                                </button>
+                            )
+                        }
+
+                        
+                    </div>
                 ))}
+                {/* mensaje de error */}
+                {
+                    showErr && <p
+                        className="contentRegister__form--contentInputErr"
+                    >
+                        {msgError}
+                    </p>
+                }
+
                 <div className="contentRegister__form--contentLink">
                     <Link href={"/"}>¿Ya tienes una cuenta?</Link>
                 </div>
