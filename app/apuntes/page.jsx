7e052a7051d118a48b95add65b3a0baab1a9bee1
el_tio_dan <xddanieloow@gmail.com>
2024-12-17 17:16:23 -1000

    Se ha puesto un mensaje de aviso para avisas que apuntes sigue en desarrollo

--- app/apuntes/page.jsx
+++ app/apuntes/page.jsx
@@ -1,6 +1,9 @@
+import MensajesModal from "@/componentes/mensajesModal/mensaje";
+
 export default function Apuntes() {
     return (
         <main>
+            <MensajesModal mensaje={`Esta parte por ahora esta en desarrollo.`}/>
             <h1>Apuntes</h1>
         </main>
     )
