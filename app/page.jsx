3650709f043f2cc43e68294424c21362f2ea9d0c
el_tio_dan <xddanieloow@gmail.com>
2024-12-17 17:18:35 -1000

    Se ha puesto un mensaje de aviso

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
+import MensajesModal from "@/componentes/mensajesModal/mensaje";
+
 export default function Home() {
     return(
         <main>
-            
+            <MensajesModal mensaje={`Por ahora esta parte esta en desarrollo.`}/>
         </main>
     )
 }
