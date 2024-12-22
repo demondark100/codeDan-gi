f8584562171080cc7d9651ac7e1c861139994f91
el_tio_dan <xddanieloow@gmail.com>
2024-12-22 08:28:00 -1000

    Se agrego un buscador en la barra de navegacion de categoria

--- src/componentes/menuCatalogo/catalogo.jsx
+++ src/componentes/menuCatalogo/catalogo.jsx
@@ -4,16 +4,36 @@ import { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./catalogo.css";
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft, faChevronRight, faX } from "@fortawesome/free-solid-svg-icons";
 
 
 
 function CatalogoNav() {
-    const [showHideNav, setShowHideNav] = useState(false);
 
+    // funciones para mostrar y ocultar el nav de cursos.
+    const [showHideNav, setShowHideNav] = useState(false);
     function showHideNavSearch() {
         setShowHideNav(!showHideNav);
     }
+
+    // funciones para mostrar y ocultar el boton de eliminar contenido de input.
+    const [showHideBtnDel, setShowHideBtnDel] = useState(false);
+
+
+    // input de busqueda
+    const [inputSearch, setInputSearch] = useState("");
+    // manejar valores de input.
+    function manejarInput(valor) {
+        setInputSearch(valor);
+        valor.length > 0 ? setShowHideBtnDel(true) : setShowHideBtnDel(false);
+    }
+    // funcion para eliminar el contenido del input de busqueda.
+    function delContentInputSearch(){
+        setInputSearch("");
+        setShowHideBtnDel(false);
+    }
+
+
 
     // const apiUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000/" : "https://tu-servidor-en-produccion.com/";
 
@@ -25,11 +45,30 @@ function CatalogoNav() {
         <div className="catalogoContent">
             
             <nav className={`navCatalogoContent ${showHideNav ? "navCatalogoContentShow" : ""}`}>
+
+                {/* buscador */}
                 <div className="navCatalogoContent__inputContent">
                     <div className="navCatalogoContent__inputContent--input">
-                        <input type="text" placeholder="Buscar"/>
+                        <input 
+                            type="text" 
+                            placeholder="Buscar"
+                            onInput={(e)=> manejarInput(e.target.value)}
+                            value={inputSearch || ""}
+                        />
+                        {
+                            showHideBtnDel && <button   
+                                className="navCatalogoContent__inputContent--input--btnDel"
+                                onClick={delContentInputSearch}
+                            >
+                                <FontAwesomeIcon icon={faX}/>
+                            </button>
+                        }
+
                     </div>
-                </div>    
+                </div>
+
+
+
             </nav>
 
 
