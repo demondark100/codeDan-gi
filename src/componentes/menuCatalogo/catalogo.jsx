c375de6bf9978cd2274ddc7aef63cdb99741d93a
el_tio_dan <xddanieloow@gmail.com>
2024-12-22 06:44:08 -1000

    Se ha agregado una estructura para el nav para un buscador y tambien se empezara a renderizar todo el curso y documentacion desde 0

--- src/componentes/menuCatalogo/catalogo.jsx
+++ src/componentes/menuCatalogo/catalogo.jsx
@@ -1,9 +1,47 @@
-function CategoriaNav() {
-    return (  
-        <aside>
+"use client";
+
+import { useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./catalogo.css";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+
+
+function CatalogoNav() {
+    const [showHideNav, setShowHideNav] = useState(false);
+
+    function showHideNavSearch() {
+        setShowHideNav(!showHideNav);
+    }
+
+    // const apiUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000/" : "https://tu-servidor-en-produccion.com/";
+
+    // let datos = fetch(apiUrl)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data));
+    
+    return ( 
+        <div className="catalogoContent">
             
-        </aside>
+            <nav className={`navCatalogoContent ${showHideNav ? "navCatalogoContentShow" : ""}`}>
+                <div className="navCatalogoContent__inputContent">
+                    <div className="navCatalogoContent__inputContent--input">
+                        <input type="text" placeholder="Buscar"/>
+                    </div>
+                </div>    
+            </nav>
+
+
+            <button 
+                className={`catalogoContent--btnOpen ${showHideNav ? "catalogoContent--btnOpenRotate" : ""}`}
+                onClick={showHideNavSearch}
+            >
+                <FontAwesomeIcon icon={faChevronRight}/>
+            </button>
+        </div>
+
     );
 }
 
-export default CategoriaNav;
+export default CatalogoNav;
