3f5f81138e9dbb35ea72acb3affc9e5a268cf418
el_tio_dan <xddanieloow@gmail.com>
2024-12-25 18:57:41 -1000

    Se ha agregado una ruta para la introduccion de html por ahora se estan probando todos los componentes que estamos creando

--- src/app/catalogo/html/capitulo_1/introduccion/page.jsx
+++ src/app/catalogo/html/capitulo_1/introduccion/page.jsx
@@ -1,8 +1,20 @@
+import Codigo from "@/componentes/cursos/codigo/codigo";
+import Texto from "@/componentes/cursos/texto/texto";
+import Titulo from "@/componentes/cursos/titulo/titulo";
+
 function IntroduccionHtml() {
     return (  
-        <>
-            pepa
-        </>
+        <main className="mainCap">
+            <Titulo level={1} text="Introducción" />
+            <Texto texto={`Pppa es un drogosaurio`}/>
+            <Codigo lenguaje={"javascript"} codigo={`console.log("Peppa")
+function pepa(){
+    console.log("pepa");
+}`}/>
+<Codigo lenguaje={"html"} codigo={`<html>
+    <p></p>
+</html>`}/>
+        </main>
     );
 }
 
