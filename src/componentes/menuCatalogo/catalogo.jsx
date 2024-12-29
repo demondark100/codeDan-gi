0b77cfedf8e43643be27de17235c65f2b766a6db
el_tio_dan <xddanieloow@gmail.com>
2024-12-28 19:15:54 -1000

    Se ha agregado una mejor forma de consultar la db en el consumno al servidor para reenderizar los capitulos en el menu de nav de catalogo

--- src/componentes/menuCatalogo/catalogo.jsx
+++ src/componentes/menuCatalogo/catalogo.jsx
@@ -1,134 +1,117 @@
 "use client";
 
 import { useState, useEffect } from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./catalogo.css";
-import { faChevronDown, faChevronLeft, faChevronRight, faChevronUp, faX } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faChevronRight, faChevronUp, faX } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
-
-
+import "./catalogo.css";
 
 function CatalogoNav() {
-
-    // funciones para mostrar y ocultar el nav de cursos.
     const [showHideNav, setShowHideNav] = useState(false);
-    function showHideNavSearch() {
-        setShowHideNav(!showHideNav);
-    }
-
-    // funciones para mostrar y ocultar el boton de eliminar contenido de input.
     const [showHideBtnDel, setShowHideBtnDel] = useState(false);
-
-
-    // input de busqueda
     const [inputSearch, setInputSearch] = useState("");
-    // manejar valores de input.
+    const [rutas, setRutas] = useState([]);
+    const [openRutaIndex, setOpenRutaIndex] = useState(null);
+    const [openCapituloIndex, setOpenCapituloIndex] = useState(null);
+
+    useEffect(() => {
+        async function getData() {
+            const apiUrl = "https://buscores-server-gi-2.onrender.com/api/catalogo";
+            const res = await fetch(apiUrl);
+            const data = await res.json();
+            setRutas(data);
+        }
+        getData();
+    }, []);
+
     function manejarInput(valor) {
         setInputSearch(valor);
-        valor.length > 0 ? setShowHideBtnDel(true) : setShowHideBtnDel(false);
+        setShowHideBtnDel(valor.length > 0);
     }
-    // funcion para eliminar el contenido del input de busqueda.
-    function delContentInputSearch(){
+
+    function delContentInputSearch() {
         setInputSearch("");
         setShowHideBtnDel(false);
     }
 
-
-    // obtener todas las rutas.
-    const [rutas, setRutas] = useState([]);
-    async function getData(){
-        // const apiUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3001/api/catalogo" : "https://buscores-server-gi-2.onrender.com/api/catalogo";
-
-        const apiUrl = "https://buscores-server-gi-2.onrender.com/api/catalogo";
-    
-        const res = await fetch(apiUrl);
-        const data = await res.json();
-        setRutas(data);
+    function toggleNav() {
+        setShowHideNav(!showHideNav);
     }
 
-    useEffect(()=>{getData()},[]);
+    function toggleRutaIndex(index) {
+        setOpenRutaIndex(openRutaIndex === index ? null : index);
+    }
 
-    // mostrar y ocultar lenguaje y capitulos.
-    const [openRutaIndex, setOpenRutaIndex] = useState(null);
-    const [openCapituloIndex, setOpenCapituloIndex] = useState(null);
-    
-    return ( 
+    function toggleCapituloIndex(index) {
+        setOpenCapituloIndex(openCapituloIndex === index ? null : index);
+    }
+
+    return (
         <div className="catalogoContent">
-            
             <nav className={`navCatalogoContent ${showHideNav ? "navCatalogoContentShow" : ""}`}>
-
-                {/* buscador */}
                 <div className="navCatalogoContent__inputContent">
                     <div className="navCatalogoContent__inputContent--input">
                         <input 
                             type="text" 
                             placeholder="Buscar"
-                            onInput={(e)=> manejarInput(e.target.value)}
-                            value={inputSearch || ""}
+                            onInput={(e) => manejarInput(e.target.value)}
+                            value={inputSearch}
                         />
-                        {
-                            showHideBtnDel && <button   
+                        {showHideBtnDel && (
+                            <button 
                                 className="navCatalogoContent__inputContent--input--btnDel"
                                 onClick={delContentInputSearch}
                             >
                                 <FontAwesomeIcon icon={faX}/>
                             </button>
-                        }
-
+                        )}
                     </div>
                 </div>
-
-                {
-                    rutas.map((ruta, rutaIndex) => (
-                        <div key={rutaIndex}>
-                            <button 
-                                onClick={() => setOpenRutaIndex(openRutaIndex === rutaIndex ? null : rutaIndex)}
-                                className="navCatalogoContent__btnRuta"
-                            >
-                                <span>
-                                    {ruta.name}
-                                    <FontAwesomeIcon icon={openRutaIndex === rutaIndex ? faChevronUp : faChevronDown}/>
-                                </span>
-                            </button>
-                            {openRutaIndex === rutaIndex && (
-                                <ul>
-                                    {
-                                        ruta.capitulos.map((capitulo, capituloIndex) => (
-                                            <li 
-                                                key={capituloIndex}
-                                                className="navCatalogoContent__capitulos"
-                                            >
-                                                <h4 
-                                                    onClick={() => setOpenCapituloIndex(openCapituloIndex === capituloIndex ? null : capituloIndex)}
-                                                    className="navCatalogoContent__lenguajes"
+                {rutas.map((ruta, rutaIndex) => (
+                    <div key={rutaIndex}>
+                        <button 
+                            onClick={() => toggleRutaIndex(rutaIndex)}
+                            className="navCatalogoContent__btnRuta"
+                        >
+                            <span>
+                                {ruta.name}
+                                <FontAwesomeIcon icon={openRutaIndex === rutaIndex ? faChevronUp : faChevronDown}/>
+                            </span>
+                        </button>
+                        {openRutaIndex === rutaIndex && (
+                            <ul>
+                                {ruta.capitulos.map((capitulo, capituloIndex) => (
+                                    <li 
+                                        key={capituloIndex}
+                                        className="navCatalogoContent__capitulos"
+                                    >
+                                        <h4 
+                                            onClick={() => toggleCapituloIndex(capituloIndex)}
+                                            className="navCatalogoContent__lenguajes"
+                                        >
+                                            Capítulo {capituloIndex + 1}
+                                        </h4>
+                                        {openCapituloIndex === capituloIndex && (
+                                            Object.values(capitulo)[0].map((link, linkIndex) => (
+                                                <Link
+                                                    key={linkIndex} 
+                                                    href={link.link}
+                                                    className="navCatalogoContent__link"
                                                 >
-                                                    capitulo {capituloIndex + 1}
-                                                </h4>
-                                                {openCapituloIndex === capituloIndex && (
-                                                    Object.values(capitulo)[0].map((link, linkIndex) => (
-                                                        <Link
-                                                            key={linkIndex} 
-                                                            href={link.link}
-                                                            className="navCatalogoContent__link"
-                                                        >{link.title}</Link>
-                                                    ))
-                                                )}
-                                            </li>
-                                        ))
-                                    }
-                                </ul>
-                            )}
-                        </div>
-                    ))
-                }
-
+                                                    {link.title}
+                                                </Link>
+                                            ))
+                                        )}
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                ))}
             </nav>
-
-
             <button 
                 className={`catalogoContent--btnOpen ${showHideNav ? "catalogoContent--btnOpenRotate" : ""}`}
-                onClick={showHideNavSearch}
+                onClick={toggleNav}
             >
                 <FontAwesomeIcon icon={faChevronRight}/>
             </button>
