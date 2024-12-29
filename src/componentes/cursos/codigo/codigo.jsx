b701896c5516e2764386d5122390544ebb227d2e
el_tio_dan <xddanieloow@gmail.com>
2024-12-29 07:33:08 -1000

    Se cambio la estructura para que se pueda agrandar la pantalla del codigo resaltado

--- src/componentes/cursos/codigo/codigo.jsx
+++ src/componentes/cursos/codigo/codigo.jsx
@@ -4,7 +4,7 @@
 import React, { useState } from 'react';
 
 // iconos
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faExpand } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // libreria de resaltado de codigo
@@ -17,10 +17,13 @@ import "./codigo.css";
 
 function Codigo({lenguaje,codigo}) {
     // variable que cambia para mostrar o ocultar boton de copiar.
-    const [showBtnCopy, setShowBtnCopy] = useState(false);
+    const [showBtns, setShowBtns] = useState(false);
 
     // estado para mostrar mensaje de copiado o no.
     const [showHideMsgCopy, setShowHideMsgCopy] = useState(false);
+
+    // expandir el codigo a pantalla completa.
+    const [isComplete, setIsComplete] = useState(false);
 
     const copiarCodigo = () => {
         navigator.clipboard.writeText(codigo)
@@ -37,18 +40,29 @@ function Codigo({lenguaje,codigo}) {
 
     return (
         <div 
-            className='codigoContent'
+            className={`codigoContent ${isComplete ? "codigoContentComplete":""}`}
             onDoubleClick={copiarCodigo}
-            onMouseOver={() => setShowBtnCopy(true)}
-            onMouseOut={() => setShowBtnCopy(false)}
+            onMouseOver={() => setShowBtns(true)}
+            onMouseOut={() => setShowBtns(false)}
         >
+            {/* boton para copiar codigo */}
             {
-                showBtnCopy &&
+                showBtns &&
                 <button 
                     className='codigo__btnCopy'
                     onClick={copiarCodigo}
                 >
                     <FontAwesomeIcon icon={faCopy} />
+                </button>
+            }
+            {/* codigo en pantalla completa */}
+            {
+                showBtns && 
+                <button
+                    className='codigo__btnExpand'
+                    onClick={()=>setIsComplete(!isComplete)}
+                >
+                    <FontAwesomeIcon icon={faExpand}/>
                 </button>
             }
 
