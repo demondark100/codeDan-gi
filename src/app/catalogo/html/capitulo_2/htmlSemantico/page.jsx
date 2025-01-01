f7d1933a71a7202aa1e74f5c642c65de5c35d43f
el_tio_dan <xddanieloow@gmail.com>
2025-01-01 07:56:54 -1000

    Se agrego un nuevo titulo para htmlsemantico como prueba para ver si se reflejan los cambios en vercel

--- src/app/catalogo/html/capitulo_2/htmlSemantico/page.jsx
+++ src/app/catalogo/html/capitulo_2/htmlSemantico/page.jsx
@@ -35,6 +35,7 @@ function HtmlSemantico() {
 </body>`
               }]}
             />
+            <Titulo level={1} text={`Ppepa`}/>
             <Titulo level={1} text={`HTML semántico`}/>
             <Texto texto={`Aquí veremos algunas etiquetas de HTML para empezar con unos cuantos proyectos.`}/>
             <Titulo level={2} text={`Títulos`}/>
